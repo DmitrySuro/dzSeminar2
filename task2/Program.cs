01ca6c0e1782271859213ada9d72f35eb3bfc193
DmitrySuro <dmitrysurodeev@yandex.ru>
2022-08-10 21:16:58 +0300

    addTask2,со случайным трехзначным письмом

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число
+// и удаляет вторую вторую цифру этого числа
+// 456-> 46
+// 782 -> 72
+// 918 ->
+
+System.Console.Clear();
+
+Random rnd = new Random();
+int number = rnd.Next(100,1000);
+  
+    System.Console.WriteLine($"Вот трехзначное случайное число {number}");
+
+int firstNumber = number / 100;
+int thirdNumber = number % 10;
+
+    System.Console.WriteLine($"Вторая цифра удалена и вот результат {firstNumber}{thirdNumber}");
